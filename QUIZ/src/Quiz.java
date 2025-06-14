b5afedce8051296c04e381050ed2e1c6cbe7af5c
Keiner-Josue <kjbarbosac@ufpso.edu.co>
2025-06-13 15:46:10 -1200

    Update Quiz.java

--- QUIZ/src/Quiz.java
+++ QUIZ/src/Quiz.java
@@ -1,18 +1,34 @@
 import java.util.Scanner;
 
-public class Quiz {
+public class SecuenciaFibonacci {
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner (System.in);
-        System.out.println("Ingrese el numero para su serie de Fibonacci: ");
-        int num1 = scanner.nextInt();
-        for(int num=0){
-            if (num1+num;i++) {
-                
-            }
+        Scanner scanner = new Scanner(System.in);
+
+        // 1. Solicitar el límite
+        System.out.print("Ingrese un número entero positivo como límite: ");
+        int limite = scanner.nextInt();
+
+        // Variables para generar la secuencia
+        int a = 0, b = 1, suma = 0, contador = 0;
+
+        System.out.println("\nSecuencia de Fibonacci hasta " + limite + ":");
+
+        // 2. Generar y mostrar secuencia
+        while (a <= limite) {
+            System.out.print(a + " ");
+            suma += a;
+            contador++;
+
+            int siguiente = a + b;
+            a = b;
+            b = siguiente;
         }
 
+        // 3. Mostrar resultados finales
+        System.out.println("\n\nCantidad de números generados: " + contador);
+        System.out.println("Suma total de los números: " + suma);
 
-
-        scanner.close ();
+        scanner.close();
     }
 }
